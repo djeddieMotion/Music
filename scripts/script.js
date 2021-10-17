bc76cd1cf286093d84c498ce068f4d21a3c9b57e
djeddieMotion <92647754+djeddieMotion@users.noreply.github.com>
2021-10-16 20:30:38 -0400

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -12,16 +12,16 @@ new Vue({
         {
           name: "A Miuda Daquele Bar",
           artist: "Jaimão",
-          cover: "https://m.media-amazon.com/images/I/81ELVyM8INL._SS500_.jpg",
+          cover: "https://raw.githubusercontent.com/djeddieMotion/music/main/img/1.jpg",
           source: "https://raw.githubusercontent.com/djeddieMotion/music/main/mp3/1.mp3",
           url: "https://www.youtube.com/watch?v=dz79L_fhAHw",
           favorited: false
         },
         {
-          name: "Everybody Knows",
-          artist: "Leonard Cohen",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/2.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/2.mp3",
+          name: "Amanhã Faço Dieta",
+          artist: "Herman José",
+          cover: "https://raw.githubusercontent.com/djeddieMotion/music/main/img/2.jpg",
+          source: "https://raw.githubusercontent.com/djeddieMotion/music/main/mp3/2.mp3",
           url: "https://www.youtube.com/watch?v=Lin-a2lTelg",
           favorited: true
 		  
@@ -29,56 +29,56 @@ new Vue({
         {
           name: "Extreme Ways",
           artist: "Moby",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/3.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/3.mp3",
+          cover: "https://raw.githubusercontent.com/djeddieMotion/music/main/img/3.jpg",
+          source: "https://raw.githubusercontent.com/djeddieMotion/music/main/mp3/3.mp3",
           url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
           favorited: false
         },
         {
           name: "Butterflies",
           artist: "Sia",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/4.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/4.mp3",
+          cover: "https://raw.githubusercontent.com/djeddieMotion/music/main/img/4.jpg",
+          source: "https://raw.githubusercontent.com/djeddieMotion/music/main/mp3/4.mp3",
           url: "https://www.youtube.com/watch?v=kYgGwWYOd9Y",
           favorited: false
         },
         {
           name: "The Final Victory",
           artist: "Haggard",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/5.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/5.mp3",
+          cover: "https://raw.githubusercontent.com/djeddieMotion/music/main/img/5.jpg",
+          source: "https://raw.githubusercontent.com/djeddieMotion/music/main/mp3/5.mp3",
           url: "https://www.youtube.com/watch?v=0WlpALnQdN8",
           favorited: true
         },
         {
           name: "Genius ft. Sia, Diplo, Labrinth",
           artist: "LSD",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/6.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/6.mp3",
+          cover: "https://raw.githubusercontent.com/djeddieMotion/music/main/img/6.jpg",
+          source: "https://raw.githubusercontent.com/djeddieMotion/music/main/mp3/6.mp3",
           url: "https://www.youtube.com/watch?v=HhoATZ1Imtw",
           favorited: false
         },
         {
           name: "The Comeback Kid",
           artist: "Lindi Ortega",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/7.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/7.mp3",
+          cover: "https://raw.githubusercontent.com/djeddieMotion/music/main/img/7.jpg",
+          source: "https://raw.githubusercontent.com/djeddieMotion/music/main/mp3/7.mp3",
           url: "https://www.youtube.com/watch?v=me6aoX0wCV8",
           favorited: true
         },
         {
           name: "Overdose",
           artist: "Grandson",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/8.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/8.mp3",
+          cover: "https://raw.githubusercontent.com/djeddieMotion/music/main/img/8.jpg",
+          source: "https://raw.githubusercontent.com/djeddieMotion/music/main/mp3/8.mp3",
           url: "https://www.youtube.com/watch?v=00-Rl3Jlx-o",
           favorited: false
         },
         {
           name: "Rag'n'Bone Man",
           artist: "Human",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/9.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/9.mp3",
+          cover: "https://raw.githubusercontent.com/djeddieMotion/music/main/img/9.jpg",
+          source: "https://raw.githubusercontent.com/djeddieMotion/music/main/mp3/9.mp3",
           url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
           favorited: false
         }
